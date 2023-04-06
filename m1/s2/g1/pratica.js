82100bb1e67986161c74a7f0397c60bb9147ea5a
Unicorn-VX <128901554+Unicorn-VX@users.noreply.github.com>
2023-04-07 00:10:20 +0200

    homewrok g1

---
+++ m1/s2/g1/pratica.js
@@ -0,0 +1,9 @@
+alert("hello world")
+
+document.getElementById("hello").innerHTML="Hello, mi chiamo Veronica Xia"
+
+var autore = prompt("Ciao, molto piacere conoscerti. Clicca sul tasto ok per entrare.")
+
+console.log(autore)
+
+
