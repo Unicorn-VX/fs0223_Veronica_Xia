7e328f2a0dbb1c3712d2a6f178c845ba235b4c63
Unicorn-VX <128901554+Unicorn-VX@users.noreply.github.com>
2023-04-07 16:58:01 +0200

    g5

---
+++ m1/s2/g5/test/test.js
@@ -0,0 +1,35 @@
+/* ESERCIZIO 1
+    Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
+*/
+const pets = ['dog', 'cat', 'hamster', 'redfish']
+/*let pet={
+    dog:"dog",
+    cat:"cat",
+    hamster:"hamster",
+    redfish:"redfish"
+}
+console.log(pet.dog)
+console.log(pet.cat)
+console.log(pet.hamster)
+console.log(pet.redfish)
+*/
+
+/* ESERCIZIO 2
+    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+*/
+/*
+pets.sort()
+console.log(pets)
+*/
+
+/* ESERCIZIO 3
+    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+*/
+/*
+const DESCPETS= pets.sort().reverse()
+console.log(DESCPETS)
+*/
+
+/* ESERCIZIO 4
+    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+*/
